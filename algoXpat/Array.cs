6286ee455f2b5659698d9b0676ac22a767b52ab1
Vandathron <scientistrabzy@gmail.com>
2021-07-11 15:43:44 +0100

    optimised tournament winner algorithm

--- algoXpat/Array.cs
+++ algoXpat/Array.cs
@@ -11,35 +11,29 @@ namespace DataStructure.algoXpat
 
 		public static string TournamentWinner(List<List<string>> competitions, List<int> results)
 		{
-			// Write your code here.
 			String winner = "";
+			int currentWinnerpoint = 0;
 			Dictionary<string, int> teamPointTable = new Dictionary<string, int>();
-
 			for (int i = 0; i < results.Count; i++)
 			{
 				int res = results[i];
 				int teamWinner = res == 1 ? 0 : 1;
-					string teamKey = competitions[i][teamWinner];
-					if (teamPointTable.ContainsKey(teamKey))
-                    {
-						teamPointTable[teamKey]+= 3;
-                    } else
-                    {
-						teamPointTable.Add(teamKey, 3);
-                    }
-			}
+				string teamKey = competitions[i][teamWinner];
+				if (teamPointTable.ContainsKey(teamKey))
+				{
+					teamPointTable[teamKey] += 3;
+				}
+				else
+				{
+					teamPointTable.Add(teamKey, 3);
+				}
 
-			int score = 0;
-            foreach (var item in teamPointTable)
-            {
-				if (item.Value > score)
-                {
-					score = item.Value;
-					winner = item.Key;
+				if (teamPointTable[teamKey] >= currentWinnerpoint)
+				{
+					winner = teamKey;
+					currentWinnerpoint = teamPointTable[teamKey];
 				}
 			}
-
-			// find winner
 			return winner;
 		}
 	}
