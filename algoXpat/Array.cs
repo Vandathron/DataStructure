8a4976715ebed716d96073ede5553d9eb57cc754
Vandathron <scientistrabzy@gmail.com>
2021-07-11 15:11:46 +0100

    TrounamentWinner

---
+++ algoXpat/Array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure.algoXpat
+{
+    public class Array
+    {
+        public Array()
+        {
+        }
+
+		public static string TournamentWinner(List<List<string>> competitions, List<int> results)
+		{
+			// Write your code here.
+			String winner = "";
+			Dictionary<string, int> teamPointTable = new Dictionary<string, int>();
+
+			for (int i = 0; i < results.Count; i++)
+			{
+				int res = results[i];
+				int teamWinner = res == 1 ? 0 : 1;
+					string teamKey = competitions[i][teamWinner];
+					if (teamPointTable.ContainsKey(teamKey))
+                    {
+						teamPointTable[teamKey]+= 3;
+                    } else
+                    {
+						teamPointTable.Add(teamKey, 3);
+                    }
+			}
+
+			int score = 0;
+            foreach (var item in teamPointTable)
+            {
+				if (item.Value > score)
+                {
+					score = item.Value;
+					winner = item.Key;
+				}
+			}
+
+			// find winner
+			return winner;
+		}
+	}
+}
